634e7ad76695c267c9aace0b6629e6004619622a
Avidor Turkewitz <avidor@flatironschool.com>
2016-06-29 18:25:14 -0400

    Solution fix (#21)
    * Update save method to return self
    
    * update spec to check that #save returns instance of Dog

--- lib/dog.rb
+++ lib/dog.rb
@@ -38,6 +38,7 @@ class Dog
       DB[:conn].execute(sql, self.name, self.breed)
       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
     end
+    self
   end
 
   def self.create(name:, breed:)
