23f892e5831a7a84213bac0d2d9f4d2724c312a3
Morgan VanYperen <morgvanny@gmail.com>
2018-06-12 16:53:24 -0400

    Update dog_spec.rb
    this test is useless if the before_each creates the table and then does not drop it before checking if that create_table method works

--- spec/dog_spec.rb
+++ spec/dog_spec.rb
@@ -39,6 +39,7 @@ describe "Dog" do
 
   describe ".create_table" do
     it 'creates the dogs table in the database' do
+      DB[:conn].execute("DROP TABLE IF EXISTS dogs")
       Dog.create_table
       table_check_sql = "SELECT tbl_name FROM sqlite_master WHERE type='table' AND tbl_name='dogs';"
       expect(DB[:conn].execute(table_check_sql)[0]).to eq(['dogs'])
