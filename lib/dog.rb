74b17547b4e8f9a0566f41ffa44621f7bd18e443
Daryl Rogony <darylrogony@gmail.com>
2022-06-16 11:35:11 +0300

    commit tree c5f2f5fa405e9afd5062084cf7fc994a83c41fe1

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,36 @@
 class Dog
 
+  attr_accessor :name, :breed, :id
+
+  def initialize(name:, breed:, id: nil)
+    @id = id
+    @name = name
+    @breed = breed
+  end
+
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS dogs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        breed TEXT
+      )
+      SQL
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = "DROP TABLE IF EXISTS dogs"
+    DB[:conn].execute(sql)
+  end
+
+  def save
+    sql = <<-SQL
+      INSERT INTO dogs (name, breed)
+      VALUES (?, ?)
+    SQL
+
+    DB[:conn].execute(sql, self.name, self.breed)
+  end
+
 end
