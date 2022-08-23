f432a9c7e4e9f4252a75ce64e885c502c54fbaf7
Silvia-Mutete <silvia.mutete@student.moringaschool.com>
2022-08-23 17:55:37 +0300

    commit tree 83060e7b4dc59ede80734d9c45a4d07a48ab4bf1

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,84 @@
 class Dog
+    attr_accessor :id, :name ,:breed
+    def initialize(id:nil, name:, breed:)
+        @id =id
+        @name=name
+        @breed=breed
+    end
+    def self.drop_table
+        sql = <<-SQL
+          DROP TABLE IF EXISTS dogs
+        SQL
+        DB[:conn].execute(sql)
+      end
 
+      def self.create_table
+        sql = <<-SQL
+          CREATE TABLE IF NOT EXISTS dogs (
+            id INTEGER PRIMARY KEY,
+            name TEXT,
+            breed TEXT
+          )
+        SQL
+        DB[:conn].execute(sql)
+      end
+
+      def save
+        sql = <<-SQL
+          INSERT INTO dogs (name, breed)
+          VALUES (?, ?)
+        SQL
+        DB[:conn].execute(sql, self.name, self.breed)
+        self.id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+        self
+      end
+
+      def self.create(name:, breed:)
+        dog = Dog.new(name: name, breed: breed)
+        dog.save
+      end
+    
+      def self.new_from_db(row)
+        self.new(id: row[0], name: row[1], breed: row[2])
+      end
+    
+      def self.all
+        sql = <<-SQL
+          SELECT *
+          FROM dogs
+        SQL
+    
+        DB[:conn].execute(sql).map do |row|
+          self.new_from_db(row)
+        end
+      end
+    
+        def self.find_by_name(name)
+          sql = <<-SQL
+            SELECT *
+            FROM dogs
+            WHERE name = ?
+            LIMIT 1
+          SQL
+    
+          DB[:conn].execute(sql, name).map do |row|
+            self.new_from_db(row)
+          end.first
+        end
+        def self.find(id)
+            sql = <<-SQL
+              SELECT *
+              FROM dogs
+              WHERE id = ?
+              LIMIT 1
+            SQL
+      
+            DB[:conn].execute(sql, id).map do |row|
+              self.new_from_db(row)
+            end.first
+          end
+      
+    
+    
+    
 end
